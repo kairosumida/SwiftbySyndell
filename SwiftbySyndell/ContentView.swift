8ae3fdfac0e6a41d530f14d8267f6d656e2a6ef9
kairosumida <kairosumida@hotmail.com>
2022-05-12 13:08:40 -0300

    Initial Commit

---
+++ SwiftbySyndell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftbySyndell
+//
+//  Created by Kairo Sumida on 12/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftbySyndell/SwiftbySyndellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftbySyndellApp.swift
+//  SwiftbySyndell
+//
+//  Created by Kairo Sumida on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftbySyndellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
