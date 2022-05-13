41d91b62b740f6f82f896e02dad84cba85488c94
kairosumida <kairosumida@hotmail.com>
2022-05-13 18:38:30 -0300

    Criado projeto

---
+++ SwiftbySyndell/SwiftUIStackAndSpacers/SwiftUIStackAndSpacers.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIStackAndSpacers.swift
+//  SwiftbySyndell
+//
+//  Created by Kairo Sumida on 13/05/22.
+//
+
+import SwiftUI
+
+struct SwiftUIStackAndSpacers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIStackAndSpacers_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIStackAndSpacers()
+    }
+}
