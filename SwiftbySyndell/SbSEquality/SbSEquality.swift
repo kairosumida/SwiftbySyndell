32bf90c7ff243920262316077b0cfaff5788474c
kairosumida <kairosumida@hotmail.com>
2022-05-16 17:18:28 -0300

    Criado Exercicio Equality

---
+++ SwiftbySyndell/SbSEquality/SbSEquality.swift
@@ -0,0 +1,20 @@
+//
+//  SbSEquality.swift
+//  SwiftbySyndell
+//
+//  Created by Kairo Sumida on 16/05/22.
+//
+
+import SwiftUI
+
+struct SbSEquality: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SbSEquality_Previews: PreviewProvider {
+    static var previews: some View {
+        SbSEquality()
+    }
+}
