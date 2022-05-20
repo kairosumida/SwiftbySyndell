bd83891405977cecd4ded0ba42cc50260201e64e
kairosumida <kairosumida@hotmail.com>
2022-05-20 19:10:48 -0300

    Create AvailabilityChecks

---
+++ SwiftbySyndell/SbSAvailabilityChecks/SbSAvailabilityChecks.swift
@@ -0,0 +1,20 @@
+//
+//  SbSAvailabilityChecks.swift
+//  SwiftbySyndell
+//
+//  Created by Kairo Sumida on 20/05/22.
+//
+
+import SwiftUI
+
+struct SbSAvailabilityChecks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SbSAvailabilityChecks_Previews: PreviewProvider {
+    static var previews: some View {
+        SbSAvailabilityChecks()
+    }
+}
