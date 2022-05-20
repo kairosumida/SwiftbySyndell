07ebcce6d04fdf1a68cf65e8e7c6a857c33ed085
kairosumida <kairosumida@hotmail.com>
2022-05-20 20:20:16 -0300

    UpdateAvailabilityCheck

--- SwiftbySyndell/SbSAvailabilityChecks/SbSAvailabilityChecks.swift
+++ SwiftbySyndell/SbSAvailabilityChecks/SbSAvailabilityChecks.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
+func checkAvailability() -> String{
+    if #available(iOS 14, *) {
+        return ">14"
+    } else {
+        return "<14"
+    }
+}
 struct SbSAvailabilityChecks: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Text(checkAvailability())
     }
 }
 
